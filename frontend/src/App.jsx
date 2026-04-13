0ed0f923af1c5679820d5757af75d9b3b3f8884a
Saif <hasansaifkhan@gmail.com>
2026-04-13 17:58:43 +0530

    ci cd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 
 function App() {
   const [messages, setMessages] = useState([])
-  const [text, setText] = useState('Hello ')
+  const [text, setText] = useState('Hello')
 
   const handleSave = async () => {
     try {
